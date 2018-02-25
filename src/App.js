0b9e88f955f6ac3adfbe05d03c8217a6487f0b74
BlazingRockStorm <rocker.gryphon@gmail.com>
2018-02-25 10:48:01 +0700

    Divide the class into files

--- src/App.js
+++ src/App.js
@@ -1,67 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class SingleResult extends React.Component {
-  render() {
-      return (
-          <li>
-              <a href= {this.props.url} target='blank'>{this.props.title}</a>
-              <p>{this.props.description}</p>
-          </li>
-      )
-  }
-}
-
-class ResultList extends React.Component {
-  render() {
-      var results = this.props.results[1].map((result, index) => {
-          return (
-              <SingleResult key={index} title={this.props.results[1][index]} description={this.props.results[2][index]} url={this.props.results[3][index]}/>
-          );
-      });
-
-      return (<ul className="out-result">{results}</ul>);
-  }
-}
-
-class SearchForm extends React.Component {
-
-  constructor() {
-      super();
-      this.state = {
-        searchTerm: ''
-      };
-  }
-
-  handleInputChange(event) {
-      this.setState({
-          searchTerm: event.target.value
-      });
-  }
-
-  handleSubmit(event) {
-      event.preventDefault();
-      let searchTerm = this.state.searchTerm.trim(); // Remove whitespace at the beginning and end.
-
-      if (!searchTerm) { // If no search term was typed, return early and do nothing.
-          return;
-      }
-
-      this.props.onSearch(searchTerm); // Execute callback
-      this.setState({ searchTerm: '' });
-  }
-
-  render() {
-      return (
-          <div className="form-group">
-              <form onSubmit={this.handleSubmit.bind(this)}>
-                  <input id="searchTerm" className="input-sm" type="text" placeholder="Search for something..." onChange={this.handleInputChange.bind(this)} value={this.state.searchTerm}/>
-              </form>
-              <button id="search" type="button" className="btn btn-primary" onClick={this.handleSubmit.bind(this)}><span className="fa fa-search"></span></button>
-          </div>
-      );
-  }
-}
+import ResultList from './result-list';
+import SearchForm from './search-form';
 
 class App extends Component {    
   constructor() {
