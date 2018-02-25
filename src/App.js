3b937bc91873e3694dd7bddd7efdfbf3996e4971
BlazingRockStorm <rocker.gryphon@gmail.com>
2018-02-25 16:42:57 +0700

    get API with jQuery

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ResultList from './result-list';
 import SearchForm from './search-form';
-
-import superagent from 'superagent';
-import jsonp from 'superagent-jsonp';
+import $ from 'jquery'; 
 
 class App extends Component {    
   constructor() {
@@ -17,19 +15,18 @@ class App extends Component {
   }
 
   handleSearch(searchTerm) {
-    superagent.get('https://en.wikipedia.org/w/api.php') // Wikipedia API call
-    .query({
-        search: searchTerm,   // The search keyword passed by SearchForm
-        action: 'opensearch', // You can use any kind of search here, they are all documented in the Wikipedia API docs
-        format: 'json'        // We want JSON data back
-    })
-    .use(jsonp) // Use the jsonp plugin
-    .end((error, response) => {
-       if (error) {
-           console.error(error);
-       } else {
-           this.setState({ results: response.body }); // Set the state once results are back
-       }
+    $.ajax({
+        type: 'GET',
+        url: 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' + searchTerm,
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: (data) => {
+            this.setState({ results: data });
+        },
+        error: function () {
+            $(".out-result").html("Error");
+        }
     });
   }
 
