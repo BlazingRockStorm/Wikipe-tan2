ac4ef7b878fdc903e469d4407460df5ad27dcd82
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-11-25 21:27:52 +0700

    Updates

---
+++ precache-manifest.424468f6753d42690eeb35ccbaf434ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44f92e139bdc9d1340f8b302ceedf86",
+    "url": "/Wikipe-tan2/index.html"
+  },
+  {
+    "revision": "e17d1c216c4adb334ded",
+    "url": "/Wikipe-tan2/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "336157e5f616ae7eb1b1",
+    "url": "/Wikipe-tan2/static/js/2.0cade41d.chunk.js"
+  },
+  {
+    "revision": "e17d1c216c4adb334ded",
+    "url": "/Wikipe-tan2/static/js/main.02421e28.chunk.js"
+  },
+  {
+    "revision": "0f76925a49a873ed2585",
+    "url": "/Wikipe-tan2/static/js/runtime-main.7447be58.js"
+  }
+]);
