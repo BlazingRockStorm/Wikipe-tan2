166b0a2c5ef05155380c5f34674108e792efaffb
BlazingRockStorm <rocker.gryphon@gmail.com>
2020-04-14 17:07:56 +0900

    add cypress

--- src/single-result.js
+++ src/single-result.js
@@ -5,7 +5,6 @@ class SingleResult extends React.Component {
         return (
             <li>
                 <a href= {this.props.url} target='blank'>{this.props.title}</a>
-                <p>{this.props.description}</p>
             </li>
         )
     }
