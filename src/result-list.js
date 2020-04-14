f30e21b3e93808adeb92256556f637ab2c606ff4
BlazingRockStorm <rocker.gryphon@gmail.com>
2020-04-14 17:07:23 +0900

    add cypress

--- src/result-list.js
+++ src/result-list.js
@@ -5,7 +5,7 @@ class ResultList extends React.Component {
     render() {
         var results = this.props.results[1].map((result, index) => {
             return (
-                <SingleResult key={index} title={this.props.results[1][index]} description={this.props.results[2][index]} url={this.props.results[3][index]}/>
+                <SingleResult key={index} title={this.props.results[1][index]} url={this.props.results[3][index]}/>
             );
         });
   
